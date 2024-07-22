341186d99b70b8c3e186dff05b5c36637c5e203b
Brandon Castillo <brajoel98@gmail.com>
2024-07-21 21:34:20 -0700

    feat(users): :sparkles: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { CommonModule } from '@/common/common.module';
 import { BankModule } from './bank/bank.module';
 import { SeedModule } from './seed/seed.module';
 import { TransactionModule } from './transaction';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommonModule, TransactionModule, BankModule, SeedModule],
+  imports: [
+    CommonModule,
+    TransactionModule,
+    BankModule,
+    SeedModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
---
+++ src/user/user.schema.ts
@@ -0,0 +1,26 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true, type: String, unique: true })
+  @ApiProperty()
+  email: string;
+
+  @Prop({ required: true, type: String })
+  @ApiProperty()
+  fullName: string;
+
+  @Prop({ required: false, type: String })
+  @ApiProperty()
+  picture: string;
+
+  @Prop({ required: false, type: String })
+  @ApiProperty()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.index({ email: 1 }, { unique: true });
